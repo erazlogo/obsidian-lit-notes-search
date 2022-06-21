88d2934b92dc99ddab2d5b71074541270bd4adee
Elena Razlogova <elena.razlogova@gmail.com>
2022-06-21 13:08:26 -0400

    Add multiple tags search

--- search.md
+++ search.md
@@ -7,17 +7,18 @@ date::
 
 note-title:: 
 comment:: 
-tag:: 
+tags:: 
 
 sortby:: date
 sortorder:: desc
 
 ```dataviewjs
-await dv.view("your-folder/your-sub-folder", { keyword: dv.current().keyword, author: dv.current().author, title: dv.current().title, publication: dv.current().publication, date: dv.current().date, notetitle: dv.current()["note-title"], comment: dv.current().comment, tag: dv.current().tag, sortby: dv.current().sortby, sortorder: dv.current().sortorder });
+await dv.view("your-folder/your-sub-folder", { });
 ```
 
 Keyword search finds a word or phrase in the entire note text + note title.
 For text fields, this is a case-insensitive phrase search. 
 Enter dates as `YYYY-MM-DD`, `<YYYY-MM-DD` and `>YYYY-MM-DD`.
-Enter tag as `#tag`.  
-Enter exact field title and asc/desc to change sort order.
+Enter tags as `#tag1 #tag2`.
+Enter exact field title and `asc/desc` to sort by field. 
+Leave sort fields blank to sort by `note-title, desc`.
--- view.js
+++ view.js
@@ -1,9 +1,18 @@
 const current = dv.current();
 const cdate = new Date(dv.current().date).getTime();
 const cdateop = !dv.current().date ? "" : (dv.current().date.toString().includes("<") ? "<" : (dv.current().date.toString().includes(">") ? ">" : "==="));
-const searchterm = dv.current().tag === null ? '"your-notes-folder"' : '"your-notes-folder" and '+dv.current().tag;
+const matchtags = (a, b) => {
+    for (const v of new Set(a)) {
+      if (
+        !b.some(e => e === v) ||
+        a.filter(e => e === v).length > b.filter(e => e === v).length
+      )
+        return false;
+    }
+    return true;
+};
 
-if (current.keyword || current.author || current.title || current.publication || current.date || current["note-title"] || current.comment || current.tag) {
+if (current.keyword || current.author || current.title || current.publication || current.date || current["note-title"] || current.comment || current.tags) {
 
     function passes(page) {
         return (!current.author || (page.author && page.author.toString().toLowerCase().includes(current.author.toLowerCase())))
@@ -12,33 +21,34 @@ if (current.keyword || current.author || current.title || current.publication ||
             && (!current.date || (page.date && eval(new Date(page.date).getTime() + cdateop + cdate)))
             && (!current["note-title"] || (page.file.name && page.file.name.toLowerCase().includes(current["note-title"].toLowerCase())))
             && (!current.comment || (page.comment && page.comment.toLowerCase().includes(current.comment.toLowerCase())))
+            && (!current.tags || matchtags(current.file.tags, page.file.tags))
             ;
     }
 
-function keyword(page) {
+    function keyword(page) {
         return (!current.keyword || (page.content && page.content.toLowerCase().includes(current.keyword.toLowerCase()))
-        || (page.notetitle && page.notetitle.toLowerCase().includes(current.keyword.toLowerCase())))
+            || (page.notetitle && page.notetitle.toLowerCase().includes(current.keyword.toLowerCase())))
+            ;
+    }
 
-}
-
-const pages = await Promise.all(
-    dv.pages(searchterm)
-    .where(passes)
-    .sort(p => p[current.sortby], current.sortorder)
-    .map(page => new Promise(async (resolve, reject) => {
-        const content = await dv.io.load(page.file.path);
-        resolve({
-            link: page.file.link,
-            author: page.author,
-            title: page.title,
-            publication: page.publication,
-            date: page.date,
-            notetitle: page.file.name,
-            comment: page.comment,
-            content
-        });
-    }))
-);
+    const pages = await Promise.all(
+        dv.pages('"01 notes"')
+        .where(passes)
+        .sort(p => p[current.sortby], current.sortorder)
+        .map(page => new Promise(async (resolve, reject) => {
+            const content = await dv.io.load(page.file.path);
+            resolve({
+                link: page.file.link,
+                author: page.author,
+                title: page.title,
+                publication: page.publication,
+                date: page.date,
+                notetitle: page.file.name,
+                comment: page.comment,
+                content
+            });
+        }))
+    );
 
     dv.table(
         ["Note", "Author", "Title", "Publication", "Date", "Comment"],
